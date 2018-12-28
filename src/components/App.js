b7cfabaa0b66a7262d533cb70bbf839932049a68
Patrick <patrickmao93@gmail.com>
2018-12-28 19:37:36 +0800

    wroking

--- src/components/App.js
+++ src/components/App.js
@@ -1,8 +1,33 @@
 import React from "react";
+import UserCreate from "./UserCreate";
+import LanguageContext from "../contexts/LanguageContext";
 
 class App extends React.Component {
+  state = { language: "english" };
+
+  onLanguageChange = language => {
+    this.setState({ language });
+  };
+
   render() {
-    return <div>App</div>;
+    return (
+      <div className="ui container">
+        <div className="">
+          Select a language:
+          <i
+            className="flag us"
+            onClick={() => this.onLanguageChange("english")}
+          />
+          <i
+            className="flag cn"
+            onClick={() => this.onLanguageChange("chinese")}
+          />
+        </div>
+        <LanguageContext.Provider value={this.state.language}>
+          <UserCreate />
+        </LanguageContext.Provider>
+      </div>
+    );
   }
 }
 
